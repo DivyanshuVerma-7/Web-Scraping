a7a21b2639ada6b786bb56442050fe1fdda46436
DivyanshuVerma-7 <b21153@students.iitmandi.ac.in>
2024-07-06 16:04:01 +0530

    Create main.py

---
+++ Amazon Web Scrape/main.py
@@ -0,0 +1,11 @@
+from product_page_info import product_page_info
+from search_page_info import search_page_info
+from csv_creation import csv_creation
+
+def main():
+    search_page_info()
+    product_page_info()
+    csv_creation()
+
+if __name__ == "__main__":
+    main()
